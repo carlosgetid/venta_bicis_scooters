8590fd195880a5ab5cf8c627c20002c70380e36f
PabloSaravia510 <akirasaravia12618@gmail.com>
2020-10-29 23:19:23 -0500

    creacion del proyecto

---
+++ Venta_Bicis_Scooters/Venta_Bicis_Scooters/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Venta_Bicis_Scooters
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Venta_Bicis_Scooters/Venta_Bicis_Scooters/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Venta_Bicis_Scooters.MvcApplication" Language="C#" %>
