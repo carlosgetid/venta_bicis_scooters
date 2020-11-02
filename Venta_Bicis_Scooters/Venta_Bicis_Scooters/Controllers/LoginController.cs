c0bf07173cb8dcd7b158a18a963369585ee7b92e
PabloSaravia510 <akirasaravia12618@gmail.com>
2020-11-02 17:27:49 -0500

    Agregando Vista Login

---
+++ Venta_Bicis_Scooters/Venta_Bicis_Scooters/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Venta_Bicis_Scooters.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
