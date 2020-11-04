179b03d6478ce5a8200940165c69f143e0b2409a
PabloSaravia510 <akirasaravia12618@gmail.com>
2020-11-03 23:01:50 -0500

    Agregando vista cliente

---
+++ Venta_Bicis_Scooters/Venta_Bicis_Scooters/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Venta_Bicis_Scooters.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult PrincipalCliente()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Venta_Bicis_Scooters/Venta_Bicis_Scooters/Scripts/caruselAbajo.js
@@ -0,0 +1,60 @@
+﻿const fila = document.querySelector('.contenedor-abajo-carrusel-abajo');
+const productos = document.querySelectorAll('.producto-abajo');
+
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+// Event Listener para la flecha derecha
+flechaDerecha.addEventListener('click', function () {
+    fila.scrollLeft += fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if (indicadorActivo.nextSibling) {
+        indicadorActivo.nextSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+// Event Listener para la flecha izquierda
+flechaIzquierda.addEventListener('click', function () {
+    fila.scrollLeft -= fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if (indicadorActivo.previousSibling) {
+        indicadorActivo.previousSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+// Paginacion
+const numeroPaginas = Math.ceil(productos.length / 6);
+for (let i = 0; i < numeroPaginas; i++) {
+    const indicador = document.createElement('button');
+
+    if (i === 0) {
+        indicador.classList.add('activo');
+    }
+
+    document.querySelector('.indicadores').appendChild(indicador);
+    indicador.addEventListener('click', (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
+
+        document.querySelector('.indicadores .activo').classList.remove('activo');
+        e.target.classList.add('activo');
+    })
+}
+
+// Hover
+productos.forEach((producto) => {
+    producto.addEventListener('mouseenter', (e) => {
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+            productos.forEach(producto => producto.classList.remove('hover'));
+            elemento.classList.add('hover');
+        }, 300);
+    });
+});
+
+fila.addEventListener('mouseleave', () => {
+    productos.forEach(producto => producto.classList.remove('hover'));
+});
