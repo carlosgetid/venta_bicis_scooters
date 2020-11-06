0c5194ab58c21a5f115a971317aab56099172a8d
paulable <carlospgp13@gmail.com>
2020-11-06 13:42:06 -0500

    variable de sesion implementada

--- Venta_Bicis_Scooters/Venta_Bicis_Scooters/SERVICE/ITrabajadorDao.cs
+++ Venta_Bicis_Scooters/Venta_Bicis_Scooters/SERVICE/ITrabajadorDao.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Venta_Bicis_Scooters.ENTITY;
 
 namespace Venta_Bicis_Scooters.SERVICE
 {
     public interface ITrabajadorDao<T>
     {
-        int BuscarTrabajador(string user, string pass);
+        Trabajador BuscarTrabajador(string user, string pass);
         int UpdateTrabajador(T t);
 
     }
